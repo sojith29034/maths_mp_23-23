80936858b38dc8057f43cc3476ac8e194d05b41d
Sojith Sunny <72255758+sojith29034@users.noreply.github.com>
2024-03-29 16:21:45 +0000

    corrected

--- maths_mp.py
+++ maths_mp.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
